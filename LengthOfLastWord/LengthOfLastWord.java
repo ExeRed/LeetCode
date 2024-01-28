e739cd8094f8417ead263847caeb83cdb1125fcd
Valentyn Bibik <94911912+ExeRed@users.noreply.github.com>
2024-01-28 21:06:38 +0100

    Length of Last Word

---
+++ LengthOfLastWord/LengthOfLastWord.java
@@ -0,0 +1,25 @@
+package org.example.LengthOfLastWord;
+
+public class LengthOfLastWord {
+    public static int lengthOfLastWord(String s) {
+        int i = s.length() - 1;
+
+        while (i >= 0 && s.charAt(i) == ' ') {
+            i--;
+        }
+
+        int lastIndex = i;
+
+        while (i >= 0 && s.charAt(i) != ' ') {
+            i--;
+        }
+
+        return lastIndex - i;
+
+    }
+
+    public static void main(String[] args) {
+        String s = "Hello   World    ";
+        System.out.println(lengthOfLastWord(s));
+    }
+}
